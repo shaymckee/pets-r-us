eb1234dbda5f9195e528f2f50e6fe328c9467537
Shannyn McKee <mckeeshannyn@gmail.com>
2022-09-11 19:53:51 -0500

    boarding and training

---
+++ views/partials/footer.js
@@ -0,0 +1,8 @@
+<!-- 
+  Title: footer.js
+  Author: Shay McKee
+  Date: 09/11/2022
+  Description: footer template for every page in Pets-R-Us 
+ -->
+
+<p>&copy; Copyright Shay McKee</p>
---
+++ views/partials/header.js
@@ -0,0 +1,24 @@
+<!-- 
+  Title: header.js
+  Author: Shay McKee
+  Date: 09/11/2022
+  Description: header template for every page in Pets-R-Us 
+ -->
+
+<!-- first nav bar with the home link -->
+<section class="topnav1">
+  <a href="/" class="petTital">Pets-R-Us</a>
+  <a class="active split" href="/" class="">Home</a>
+</section>
+
+<!-- second nav bar with image background -->
+<section class="bg-img">
+  <div class="containerbar">
+    <div class="topnav">
+      <button class="navbtn"><a href="/">Home</a></button>
+      <button class="navbtn"><a href="boarding">Boarding</a></button>
+      <button class="navbtn"><a href="grooming">Grooming</a></button>
+      <button class="navbtn"><a href="training">Training</a></button>
+    </div>
+  </div>
+</section>
